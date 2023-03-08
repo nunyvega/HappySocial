d9763ae146d64a6355c744d1b6327e59be308250
nunyvega <nunyvega@hotmail.com>
2023-03-08 14:10:10 +0100

    Finished step 2, working settings

--- env/src/coreApp/models.py
+++ env/src/coreApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(max_length=500, blank=True)
+    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.jpg')
+    location = models.CharField(max_length=80, blank=True)
+
+    def __str__(self):
+        return self.user.username
--- env/src/coreApp/views.py
+++ env/src/coreApp/views.py
@@ -1,6 +1,97 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 # Create your views here.
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username taken')
+                return redirect('signup')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+            # Log user in and redirect to settings
+                user = auth.authenticate(username=username, password=password)
+                auth.login(request, user)
+                return redirect('settings')
+
+            # Create profile object for the user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile(user=user_model, id_user=user_model.id)   
+                new_profile.save()
+                return redirect('signup')
+        else:
+            messages.info(request, 'Password not matching')
+            return redirect('signup')        
+
+    else:
+        return render(request, 'signup.html')
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Invalid credentials')
+            return redirect('signin')
+
+    else:
+        return render(request, 'signin.html')
+
+def signout(request):
+    auth.logout(request)
+    return redirect('signin')
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+    
+        return redirect('settings')
+
+    return render(request, 'settings.html', {'user_profile': user_profile})
